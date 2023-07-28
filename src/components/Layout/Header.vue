45dc85ac3f1f38d504d38bc83972b944e51669cf
AngelArevalo <aarevalo@dvconsultores.com>
2023-07-28 17:07:10 -0400

    Arreglo

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -67,6 +67,30 @@
                           ></v-text-field>
                         </v-col>
                       </v-row>
+                      <v-col cols="12">
+                        <v-tooltip right v-if="!user.email">
+                          <template v-slot:activator="{ on, attrs }">
+                            <v-btn
+                              plain text
+                              tile link
+                              class="no-recovery"
+                              v-bind="attrs"
+                              v-on="on"
+                            >
+                              Forgot my password
+                            </v-btn>
+                          </template>
+                          <span>First insert you email.</span>
+                        </v-tooltip>
+                        <v-btn
+                          v-else
+                          plain text
+                          tile link
+                          @click="requestRecovery"
+                        >
+                          Forgot my password
+                        </v-btn>
+                      </v-col>
                     </v-container>
                     <small>*indicates required field</small>
                     <v-btn
@@ -76,6 +100,7 @@
                       block
                       class="b1 h8-em mt-2"
                       color="#D8D8D8"
+                      type="submit"
                       >Login
                       <template v-slot:loader>
                         <span class="custom-loader">
@@ -94,7 +119,7 @@
                   >
                     Close
                   </v-btn>
-                  <v-btn class="b1 h8-em" color="green" @click="SignUp">
+                  <v-btn class="b1 h8-em" color="green" @click="signUp">
                     Sign Up
                   </v-btn>
                 </v-card-actions>
@@ -212,10 +237,23 @@
               </v-btn>
             </template>
           </v-snackbar>
-          <aside class="contright" v-if="username">
-            <span>
-              {{username}}
-            </span>
+          <aside class="contright" v-if="token">
+            <v-menu offset-y>
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn v-bind="attrs" v-on="on">
+                  {{username}}
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="item in [{id: 0, action: 'Logout'}]"
+                  :key="item.id"
+                  @click="logOut()"
+                >
+                  <v-list-item-title>{{item.action}}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           </aside>
           <aside class="contright" v-else>
             <v-btn class="walletButton" color="#656565" @click="dialog_login = true">
@@ -279,12 +317,20 @@
             /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
             "E-mail must be valid",
         ],
+        interval: null,
+        token: null
       };
     },
     mounted() {
       this.addToken(localStorage.getItem('Authorization'));
+      this.verifyStatus();
+      this.interval = setInterval(() => {this.verifyStatus()}, 10000); 
     },
     methods: {
+      verifyStatus() {
+        this.username = localStorage.getItem('Username');
+        this.token = localStorage.getItem('Authorization');
+      },
       ShowDrawer() {
         this.$refs.menu.ShowDrawer();
       },
@@ -312,6 +358,7 @@
         this.overlay = false;
         this.closeDialogs();
         this.$refs.form.reset();
+        this.verifyStatus();
       },
       postLogin() {
         if (this.$refs.form.validate()) {
@@ -320,7 +367,7 @@
             localStorage.setItem('Authorization', res.data.token)
             localStorage.setItem('Username', res.data.username)
             this.addToken(localStorage.getItem('Authorization'));
-            this.finalizePost("Welcome :" + this.user.email, "success")
+            this.finalizePost("Welcome: " + this.user.email, "success")
           }).catch((error) => {
             this.finalizePost(error.response.data, "error")
           })
@@ -339,14 +386,30 @@
           })
         }
       },
-      SignUp() {
+      signUp() {
         this.dialog_login = false;
         this.dialog_register = true;
+      },
+      logOut() {
+        clearInterval(this.interval);
+        localStorage.removeItem('Authorization');
+        localStorage.removeItem('Username');
+        this.axios.defaults.headers.common.Authorization = null;
+        this.verifyStatus();
       },
       closeDialogs() {
         this.dialog_login = false;
         this.dialog_register = false;
       },
+      requestRecovery() {
+        this.axios.post("api/recovery-email/", this.user.email).then(() => {
+          this.snackbar = true;
+          this.text = "We have sent a recovery message to your email.";
+          this.color = "success";
+        }).catch(() => {
+          this.finalizePost("An error has occured.", "error")
+        })
+      }
     },
   };
 </script>
--- src/pages/Home/NewCollections.vue
+++ src/pages/Home/NewCollections.vue
@@ -14,13 +14,13 @@
         :key="index"
         color="var(--colorCartas3)"
       >
-        <img class="images" :src="object.url_object  " alt="collections images" />
+        <img class="images" :src="object.image" :alt="'Collection Image #' + index"/>
         <a href="#" class="tl h6">{{ object.title }}</a>
         <aside class="sectdown divcol">
           <div class="space">
             <span class="h7 weight">Bid</span>
             <div class="divrow acenter">
-              <span class="price h7">$ {{ object.last_bid }} Dollars</span>
+              <!-- <span class="price h7">$ {{ object.last_bid.value }} Dollars</span> -->
             </div>
           </div>
           <span class="h7 weight tend"> Minimun Bid $ {{ object.init_price }} </span>
@@ -52,7 +52,7 @@
                     class="bg-grey-lighten-2"
                     width="550"
                     :aspect-ratio="1"
-                    :src="selected.url_object"
+                    :src="selected.image"
                     cover
                   ></v-img>
                 </v-col>
@@ -93,13 +93,21 @@
                                 class="mt-8"
                               >
                                 <v-text-field
+                                  v-if="selected.last_bid.user"
                                   v-model="bid.value"
-                                  :label="'Minimun bid ($' + selected.last_bid + ')'"
-                                  :rules="[rules.required, rules.loanMin]"
+                                  :label="'Minimun bid ($' + (selected.last_bid.value+100) + ')' "
+                                  :rules="[rules.required, rules.loanMinLast]"
                                   type="number"
                                 ></v-text-field>
-
+                                <v-text-field
+                                  v-else
+                                  v-model="bid.value"
+                                  :label="'Minimun bid ($' + (selected.last_bid.value) + ')' "
+                                  :rules="[rules.required, rules.loanMinInit]"
+                                  type="number"
+                                ></v-text-field>
                                 <v-btn
+                                  v-if="autorized"
                                   :disabled="!valid"
                                   @click="saveForm"
                                   :loading="loading"
@@ -113,6 +121,14 @@
                                     </span>
                                   </template>
                                 </v-btn>
+                                <v-btn
+                                  v-else
+                                  block
+                                  class="b1 h8-em mt-2"
+                                  color="#D8D8D8"
+                                  @click="dialog_login = true, overlay = true"
+                                  >First login
+                                </v-btn>
                               </v-form>
                             </div>
                           </span>
@@ -122,17 +138,17 @@
                         <v-card color="basil" flat>
                           <v-card-text style="text-align: justify">
                             <v-list three-line>
-                              <template v-for="item in bids">
-                                <v-list-item :key="item.id">
+                              <template v-for="(item, index) in bids">
+                                <v-list-item :key="index">
                                   <v-list-item-avatar>
-                                    <v-img :src="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfeoxz_Y3zQGazrjq06VFS37K5y8Ls-tXK1ROWFM_DRQ&s'"></v-img>
+                                    <v-img v-if="item.id" :src="'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqeZqt2CDVJaTxi_7N4q8SGxsLieLZ6QbcwolAREkfjdQS5QtVpU6N9zsL09WA8TV4ZwU&usqp=CAU'"></v-img>
                                   </v-list-item-avatar>
 
                                   <v-list-item-content>
                                     <v-list-item-title>
-                                      {{item.user}}
+                                      <span>{{ item.user }}</span>
                                     </v-list-item-title>
-                                    <v-list-item-subtitle>
+                                    <v-list-item-subtitle v-if="item.id">
                                       <span class="text--primary">Offered</span> &mdash; $ {{item.value}}
                                     </v-list-item-subtitle>
                                   </v-list-item-content>
@@ -151,6 +167,76 @@
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn class="b1 h8-em" color="#D8D8D8" @click="close">
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog v-model="dialog_login" persistent max-width="600px">
+        <v-overlay z-index="5" color="black" :value="overlay_login">
+          <v-progress-circular
+            indeterminate
+            size="64"
+          ></v-progress-circular>
+          Loading...
+        </v-overlay>
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">Login</span>
+          </v-card-title>
+          <v-card-text>
+            <v-form
+              ref="form"
+              v-model="valid"
+              lazy-validation
+              class="mt-8"
+            >
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Email*"
+                      v-model="user.email"
+                      :rules="emailRules"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Password*"
+                      type="password"
+                      v-model="user.passwd"
+                      :rules="passwdRules"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <small>*indicates required field</small>
+              <v-btn
+                :disabled="!valid"
+                @click="postLogin"
+                :loading="loading"
+                block
+                class="b1 h8-em mt-2"
+                color="#D8D8D8"
+                type="submit"
+                >Login
+                <template v-slot:loader>
+                  <span class="custom-loader">
+                    <v-icon light>mdi-cached</v-icon>
+                  </span>
+                </template>
+              </v-btn>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              class="b1 h8-em"
+              color="#D8D8D8"
+              @click="closeDialogs"
+            >
               Close
             </v-btn>
           </v-card-actions>
@@ -181,76 +267,79 @@
       return {
         loading: true,
         dialog: false,
+        dialog_login: false,
+        user: {
+          passwd: "",
+          first_name: "",
+          last_name: "",
+          email: "",
+          number: "",
+        },
+        passwdRules: [
+          (v) => !!v || "Required",
+        ],
+        emailRules: [
+          (v) =>
+            /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) ||
+            "E-mail must be valid",
+        ],
         snackbar: false,
         overlay: false,
+        overlay_login: false,
         valid: true,
         text: "Bid placed successfully",
         bid: {value: 0},
         color: "success",
         rules: {
           required: (value) => !!value || "Required.",
-          loanMin: (value) =>
-            value > this.selected.last_bid || "Bid should be above $" + this.selected.last_bid,
+          loanMinLast: (value) => (value >= this.selected.last_bid.value+100) || "Bid should be above $" + (this.selected.last_bid.value+100),
+          loanMinInit: (value) => (value >= this.selected.last_bid.value) || "Bid should be above $" + (this.selected.last_bid.value),
         },
         img: null,
         price: 0,
         tab: null,
         items: ["Details", "Bids"],
-        bids: [
-          {
-            avatar: "https://cdn.vuetifyjs.com/images/lists/1.jpg",
-            title: "adominguez@gmail.com",
-            subtitle: ``,
-          },
-          { divider: true, inset: true },
-          {
-            avatar: "https://cdn.vuetifyjs.com/images/lists/2.jpg",
-            title: "adominguez1@gmail.com",
-            subtitle: `<span class="text--primary">Offered</span> &mdash; $ 348`,
-          },
-          { divider: true, inset: true },
-          {
-            avatar: "https://cdn.vuetifyjs.com/images/lists/3.jpg",
-            title: "adominguez21@gmail.com",
-            subtitle: `<span class="text--primary">Offered</span> &mdash; $ 346`,
-          },
-          { divider: true, inset: true },
-          {
-            avatar: "https://cdn.vuetifyjs.com/images/lists/4.jpg",
-            title: "adominguez21@gmail.com",
-            subtitle: `<span class="text--primary">Offered</span> &mdash; $ 345`,
-          },
-        ],
-        text1:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-        selected: {},
-        dataNewCollections: [{},],
+        bids: [{user: "Loading...", id: null, value: null}],
+        selected: {last_bid: {value: 0, user: null}},
+        dataNewCollections: [{}],
         interval_auction: null,
+        interval_bid: null,
+        autorized: false,
+        message_if_login: "",
       };
     },
     mounted() {
       this.loading = true;
+      this.fetchAuctions();
       this.interval_auction = setInterval(() => {this.fetchAuctions()}, 3000);
     },
     methods: {
       fetchAuctions() {
+        this.autorized = localStorage.getItem('Authorization') ? true : false;
         this.axios.get("api/auction").then((res) => {
           this.dataNewCollections[0].collection = res.data;
           this.loading = false;
         })
       },
       fetchBids() {
-        this.axios.get("api/bid/?auction_id="+this.selected.id).then((res) => {
-          this.bids = res.data;
-        })
+        if (localStorage.getItem('Authorization')) {
+          this.axios.get("api/bid/?auction_id="+this.selected.id).then((res) => {
+            this.bids = res.data[0].id ? res.data : [{user: "Loading...", id: null, value: null}];
+          })
+        }
       },
       saveForm() {
         if (this.$refs.form.validate()) {
           this.overlay = true;
           this.axios.post("api/bid/", this.bid).then((res) => {
             this.bid = res.data
-            this.finalizePost("Bid placed successfully", "success");
+            this.finalizePost(this.message_if_login+"Bid placed successfully", "success");
           }).catch((error) => {
-            this.finalizePost(error.response.data, "error");
+            if (error.response.data.detail) {
+              this.finalizePost(error.response.data.detail, "error");
+            } else {
+              this.finalizePost(error.response.data, "error");
+            }
           })
         }
       },
@@ -260,22 +349,52 @@
         // Success or error dependeing on result
         this.color = color;
         this.dialog = false;
+        this.dialog_login = false;
         this.snackbar = true;
         this.overlay = false;
         this.$refs.form.reset();
+        this.message_if_login = "";
       },
       openBid(auction) {
         this.dialog = true;
         this.selected = auction;
         this.interval_bid = setInterval(() => {this.fetchBids()}, 3000);
-        this.bid = {value: auction.last_bid + 1, email: '', auction: auction.id};
+        if (auction.last_bid.user) {
+          this.bid = {value: auction.last_bid.value+100, email: '', auction: auction.id};
+        } else {
+          this.bid = {value: auction.last_bid.value, email: '', auction: auction.id};
+        }
       },
       close() {
         this.dialog = false;
         this.$refs.form.reset();
       },
+      addToken(value) {
+        if (value) {
+          this.axios.defaults.headers.common.Authorization = 'token ' + value;
+        }
+      },
+      postLogin() {
+        this.dialog_login = true;
+        this.axios.post("api/login/", {"email": this.user.email, "password": this.user.passwd}).then((res) => {
+          localStorage.setItem('Authorization', res.data.token);
+          localStorage.setItem('Username', res.data.username);
+          this.addToken(localStorage.getItem('Authorization'));
+          this.message_if_login = "Login and ";
+          this.saveForm();
+        }).catch((error) => {
+          this.finalizePost(error.response.data, "error")
+        })
+      },
+      closeDialogs() {
+        this.dialog_login = false;
+      },
     },
   };
 </script>
 
-<style src="../Pages.scss" lang="scss" />
+<style src="../Pages.scss" lang="scss">
+  .cancelar:hover {
+    cursor: pointer
+  }
+</style>
