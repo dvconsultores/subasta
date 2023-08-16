b5485309eb230f724e614af4aa0d477f4cef5a09
dvconsultores <adominguez@dvconsultores.com>
2023-08-16 10:52:40 -0400

    Agregando mail y telegram

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -10,10 +10,10 @@
               <a class="divpadd paddleftdel marginright" href="https://www.instagram.com/laraalcantara/" target="_blank">
                 <img src="@/assets/icons/instagram.svg" style="width: clamp(2em, 2vw, 3em)">
               </a>
-              <a class="divpadd paddleftdel marginright" href="https://www.instagram.com/laraalcantara/" target="_blank">
+              <a class="divpadd paddleftdel marginright" href="mailto:nftauction@dvconsultores.com" target="_blank">
                 <img src="@/assets/icons/gmail.svg" style="width: clamp(2em, 2vw, 3em)">
               </a>
-              <a class="divpadd paddleftdel marginright" href="https://www.instagram.com/laraalcantara/" target="_blank">
+              <a class="divpadd paddleftdel marginright" href="https://t.me/NearFritz" target="_blank">
                 <img src="@/assets/icons/telegram.svg" style="width: clamp(2em, 2vw, 3em)">
               </a>
             </div>
